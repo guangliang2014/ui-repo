100b15325ce826acdfc43453abefd521d48218df
unknown <rguerra@RGUERRA7010.revelex.com>
2014-07-22 16:12:45 -0400

    change for grunt

--- Gruntfile.js
+++ Gruntfile.js
@@ -6,46 +6,31 @@ module.exports = function(grunt) {
 			pkg: grunt.file.readJSON('package.json'),
 			concurrent: {
 				target: {
-					tasks: ['nodemon', 'watch'],
+					tasks: ['nodemon','compass'],
 					options: {
 						logConcurrentOutput: true
 					}
 				}
 			},
-			sass: {
-				dist: {
-					files: {
-						'public/css/style.css': 'public/sass/style.scss'
-					}
-				}
-			},
-			watch: {
-				css: {
-					files: '**/*.scss',
-					tasks: ['sass']
-				},
-				js: {
-					files: 'public/js/*.js',
-					tasks: ['uglify']
-				}
-			},
+            compass: {
+                dist: {
+                  options: {
+                    config: 'public/config.rb',
+                    basePath : 'public/',
+                    watch : true  
+                  }
+                }
+            },
 			nodemon: {
 				dev: {
 					script: 'app.js'
 				}
-			},
-			uglify: {
-				min: {
-					files: {
-						'public/js/min/helpers.min.js': ['public/js/*.js']
-					}
-				}
-			}	
-			}); 
-	grunt.loadNpmTasks('grunt-contrib-sass'); 
-	grunt.loadNpmTasks('grunt-contrib-watch');
+			}
+    }); 
+	
 	grunt.loadNpmTasks('grunt-nodemon'); 
 	grunt.loadNpmTasks('grunt-concurrent'); 
-	grunt.loadNpmTasks('grunt-contrib-uglify'); 
-	grunt.registerTask('default', ['concurrent:target']);
-	} // End wrapper function
+	grunt.loadNpmTasks('grunt-contrib-compass');
+    
+    grunt.registerTask('default', ['concurrent:target']);
+} // End wrapper function
